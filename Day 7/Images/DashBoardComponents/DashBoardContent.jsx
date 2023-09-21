4c12037f8250d196c4e82574dc2a420cc6048a1e
Thamarai Kumar <110532368+ThamaraiKumar@users.noreply.github.com>
2023-09-21 10:04:19 +0530

    Day 7 DashBoard and SideBar

---
+++ Day 7/Images/DashBoardComponents/DashBoardContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from '../Components/SideBar'
+
+function DashBoardContent() {
+  return (
+    <div>
+    <SideBar/>
+    <h1>DashBoardContent</h1></div>
+  )
+}
+
+export default DashBoardContent
