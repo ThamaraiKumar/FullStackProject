4631024c00a929ec6e44158dc1ee3113b8bb4dd8
Thamarai Kumar <110532368+ThamaraiKumar@users.noreply.github.com>
2023-09-21 10:02:59 +0530

    Day 5 using redux

---
+++ Day 5/Images/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+    <footer>
+    <div className='footer'></div>
+    
+    </footer> 
+    </div>
+  )
+}
+
+export default Footer
