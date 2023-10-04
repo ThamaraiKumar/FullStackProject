569bbc4b304ba7b19c66e5ed4b0c4e282e33dc3c
Thamarai Kumar <110532368+ThamaraiKumar@users.noreply.github.com>
2023-10-04 14:45:04 +0530

    Day 11-CRUD(BackEnd)

---
+++ Day 11/src/main/java/com/example/thamarai/academicPerformance/Controller/StaffController.java
@@ -0,0 +1,55 @@
+package com.example.thamarai.academicPerformance.Controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.thamarai.academicPerformance.Entity.StaffEntity;
+import com.example.thamarai.academicPerformance.Service.StaffService;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffController {
+	 
+	
+	@Autowired
+	StaffService staffService;
+	
+	@PostMapping("/signupStaff")
+	 public String singUp(@RequestBody StaffEntity staff)
+	  {
+		   return staffService.saveSignUp(staff);
+		  
+	  }
+	@GetMapping("/getstaff/{emailid}")
+	public Optional<StaffEntity> getStaffIn(@PathVariable String emailid)
+	{
+		return staffService.getStaff(emailid);
+	}
+	
+	
+	@GetMapping("/getAllstaff")
+	public Iterable<StaffEntity> getStaff()
+	{
+		return staffService.read();
+	}
+	
+	@PutMapping("/resetpasswordStaff")
+	public void changePassword(@RequestBody StaffEntity staff)
+	{
+		staffService.resetPassword(staff);
+	}
+	@DeleteMapping("/deleteStaff/{emailid}")
+	public void remove(@PathVariable String emailid)
+	{
+		staffService.deleteStaff(emailid);
+	}
+}
