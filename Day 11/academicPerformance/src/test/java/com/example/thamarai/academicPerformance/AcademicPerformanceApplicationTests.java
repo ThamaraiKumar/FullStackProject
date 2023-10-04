b89f262acd71e0e557090c2528ab2ec4f86cd826
Thamarai Kumar <110532368+ThamaraiKumar@users.noreply.github.com>
2023-10-04 14:40:18 +0530

    Day 11 - BackEnd(CRUD)

---
+++ Day 11/academicPerformance/src/test/java/com/example/thamarai/academicPerformance/AcademicPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thamarai.academicPerformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
